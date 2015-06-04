5393cfc7ef310e9320ca64537cdd66acfb85baf6
nrjjones <nrjjones@gmail.com>
2015-06-04 14:47:25 -0500

    Add letters and loop

---
+++ loop.R
@@ -0,0 +1,18 @@
+# To process the loop – loop.r
+
+    ## Packages
+    library(knitr)
+    library(rmarkdown)
+
+    ## Data
+    personalized_info <- read.csv(file = “meeting_times.csv”)
+
+    ## Loop
+    for (i in1:nrow(personalized_info)){
+        rmarkdown::render(input = “mail_merge_handout.Rmd”,
+        output_format = “pdf_document”,
+        output_file = paste(“handout_”, i, “.pdf”, sep=”),
+        output_dir = “handouts/”)
+    }
+
+
